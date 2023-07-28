28ea6426c1024863d9aa51975f3470019bb4d968
SkyeYoung <iskyex@outlook.com>
2023-07-28 15:45:14 +0800

    chore: 1

--- packages/provider/app/layout.tsx
+++ packages/provider/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-// trigger version
+// trigger version 1
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
