aa9fef9d3030d8b4f456d5df16df9557632ab2e6
SkyeYoung <iskyex@outlook.com>
2023-07-28 15:57:35 +0800

    fix

--- packages/provider/app/page.tsx
+++ packages/provider/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-
+// why not change this
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
